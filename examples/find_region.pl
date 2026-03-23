15c26867d77da8664fab258ea5d503cec589991d
brian d foy <briandfoy@pobox.com>
2026-03-23 05:40:29 -0400

    Nicer output for the find_region.pl

--- examples/find_region.pl
+++ examples/find_region.pl
@@ -1,5 +1,5 @@
 #!perl
-use v5.10;
+use v5.38;
 use lib qw(lib);
 
 use eBird;
@@ -8,13 +8,16 @@ use eBird::Nearby;
 
 my $ebird   = eBird->new;
 my $latlong = eBird::LatLong->new_from_decimal( 40.748817, -73.985428 );
-say "LATLONG: " . dumper( $latlong );
 
 my $nearby = eBird::Nearby->new( $latlong, $ebird );
-say "NEARBY: " . dumper( $nearby );
 
-my $region = $nearby->region;
-say "REGION: " . dumper($region);
+my $regions = $nearby->region;
+
+foreach my $region ( values $regions->%* ) {
+	my $d = $latlong->distance_to( $region );
+	printf "%4d %s   | %s\n", $d, $region->code, $region->label;
+
+	}
 
 sub dumper { state $rc = require Data::Dumper; Data::Dumper->new([@_])->Indent(1)->Sortkeys(1)->Terse(1)->Useqq(1)->Dump }
 
