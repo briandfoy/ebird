e2d10d7b7d10d9037e0df708039775c1ca49ef04
brian d foy <briandfoy@pobox.com>
2026-03-23 05:40:45 -0400

    data module for TaxonomyVersion

---
+++ lib/eBird/Data/TaxonomyVersion.pm
@@ -0,0 +1,46 @@
+use v5.38;
+use utf8;
+no feature qw(module_true);
+
+package eBird::Data::TaxonomyVersion;
+use parent qw(eBird::Data::Base);
+
+=encoding utf8
+
+=head1 NAME
+
+eBird::Data::TaxonomyVersion -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+These objects represent the countries that eBird understands.
+
+=over 4
+
+=back
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/ebird
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2023-2026, brian d foy, All Rights Reserved.
+
+You may use this code under the terms of the Artistic License 2.0.
+
+The eBird API and its data have their own terms of use:
+https://www.birds.cornell.edu/home/ebird-api-terms-of-use/
+
+=cut
+
+__PACKAGE__;
