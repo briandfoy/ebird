2c070db582411e18716d830d2f7c381c041f4ce0
brian d foy <briandfoy@pobox.com>
2026-03-24 05:38:09 -0400

    explain the different types of taxa

--- lib/eBird/Data/Taxon.pm
+++ lib/eBird/Data/Taxon.pm
@@ -9,15 +9,40 @@ use parent qw(eBird::Data::Base);
 
 =head1 NAME
 
-eBird::Data::Taxon -
+eBird::Data::Taxon - represents a single bird type (species, subspecies, group)
 
 =head1 SYNOPSIS
 
+These objects are created from the results of the API calls, and you typically
+don't need to create them yourselves:
+
+
+
 =head1 DESCRIPTION
+
+=head2 Categories
+
+There are several types of taxa:
 
 =over 4
 
+=item * spuh:  a group of species (such as genus) that is not further specified (for example, "Larus sp.")
+
+=item * slash: a species-pair, e.g., Tundra/Trumpeter Swan Cygnus columbianus/buccinator
+
+=item * species: a particular species
+
+=item * issf (Identifiable Sub-specific Group): a species
+
+=item * hybrid: offspring from the mating between two different species
+
+=item * intergrade: hybrid between two ISSF
+
+=item * form: a catch-all category
+
 =back
+
+See L<https://science.ebird.org/en/use-ebird-data/the-ebird-taxonomy>
 
 =head1 SOURCE AVAILABILITY
 
