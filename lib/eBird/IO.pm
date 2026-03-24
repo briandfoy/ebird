25ab0de67746ef12da298804aaf452b67103554b
brian d foy <briandfoy@pobox.com>
2026-03-24 05:39:52 -0400

    Fill out the IO class to make it much easier to use

--- lib/eBird/IO.pm
+++ lib/eBird/IO.pm
@@ -4,6 +4,9 @@ no feature qw(module_true);
 
 package eBird::IO;
 
+use namespace::autoclean;
+use Carp qw();
+
 =encoding utf8
 
 =head1 NAME
@@ -12,26 +15,108 @@ eBird::IO - generalize the output
 
 =head1 SYNOPSIS
 
+	use eBird;
+
+Accept the defaults, which uses C<STDOUT> and C<STDERR> and is not quiet:
+
+	my $io = eBird::IO->new();
+
+	# new line is always appended
+	$io->output( "We did something" );
+	$io->error( "There was an error" );
+
+	# this calls C<shortmess>
+	$io->carp( "message like Carp::carp" );
+
+Same, but quiet so no output is created:
+
+	my $io = eBird::IO->new( quiet => 1 );
+
+Change C<quiet> (or not) for a bit:
+
+	$io->turn_off_quiet;  # no output
+	$io->turn_on_quiet;   # can output again
+
+Supply your own filehandles, which are any object that can respond to C<print>:
+
+	my $io = eBird::IO->new(
+		error_fh  => $fh,
+		output_fh => $other_fh
+		);
+
+If you really want to be quiet and will never not be quiet. This version uses
+internal filehandles that don't do anything even if C<quiet> is false:
+
+	my $io = eBird->new_quiet;
+
 =head1 DESCRIPTION
 
 =head2 Class methods
 
 =over 4
 
-=item * new()
+=item * new(%args)
+
+=over 4
+
+
+=item * C<error_fh> - any object that can respond to C<print> (default: C<STDERR> with UTF-8)
+
+=item * C<output_fh> - any object that can respond to C<print> (default: C<STDOUT> with UTF-8)
+
+=item * C<quiet> - any object that can respond to C<print> (default: C<STDOUT>)
+
+=item * C<line_ending> - the character to add to the end of any message (default: "\n")
+
+=back
 
 =cut
 
 sub new ( $class, %args ) {
-	binmode STDOUT, ':encoding(UTF-8)';
-	binmode STDERR, ':encoding(UTF-8)';
+	state $rc = do {
+		binmode STDOUT, ':encoding(UTF-8)';
+		binmode STDERR, ':encoding(UTF-8)';
+		};
+	state $defaults = {
+		error_fh    => \*STDERR,
+		output_fh   => \*STDOUT,
+		quiet       => 0,
+		line_ending => "\n",
+		};
+	state $allowed = { map { $_, 1 } keys $defaults->%* };
 
-	my %hash = (
-		error  => \*STDERR,
-		output => \*STDOUT,
+	my %pass_through =
+		map { $_ => $args{$_} }
+		grep { exists $allowed->{$_} }
+		keys %args;
+	$pass_through{'quiet'} = 0 + !! $pass_through{'quiet'};
+
+	my %hash = ( $defaults->%*, %pass_through );
+
+	bless \%hash, $class;
+	}
+
+=item * new_quiet
+
+Returns an C<eBird::IO> object where all filehandles are L<IO::Null> handles
+and C<quiet> is true.
+
+=cut
+
+package eBird::IO::Null {
+	sub new { bless {}, __PACKAGE__ }
+	sub print { 1 }
+	}
+
+sub new_quiet ($class, %args) {
+	state $null_fh = eBird::IO::Null->new;
+	state $obj = $class->new(
+		error_fh  => $null_fh,
+		output_fh => $null_fh,
+		quiet     => 1,
 		);
 
-	my $self = bless \%hash, $class;
+	return $obj;
 	}
 
 =back
@@ -40,35 +125,95 @@ sub new ( $class, %args ) {
 
 =over 4
 
-=item * error( ARRAY_OF_STRINGS )
+=item * carp( ARRAY_OF_STRINGS )
 
 Join ARRAY_OF_STRINGS with a newline and send to the error filehandle.
 
 =cut
 
-sub error ( $self, @strings ) {
-	$self->_send_it( $self->_error_fh, join "\n", @strings );
+sub carp ( $self, @strings ) {
+	$self->send_it( $self->error_fh, Carp::shortmess(join "\n", @strings) =~ s/\n+\z//r );
 	}
+
+
+=item * error( ARRAY_OF_STRINGS )
+
+Join ARRAY_OF_STRINGS with a newline and send to the error filehandle.
+
+=item * error_fh
+
+Returns the filehandle that C<error> and C<carp> will use.
+
+=cut
+
+sub error ( $self, @strings ) {
+	$self->send_it( $self->error_fh, join "\n", @strings );
+	}
+
+sub error_fh ( $self ) { $self->{error_fh} }
 
 =item * output( ARRAY_OF_STRINGS )
 
 Join ARRAY_OF_STRINGS with a newline and send to the standard filehandle.
 
+=item * output_fh
+
+Returns the filehandle that C<output> will use.
+
 =cut
 
 sub output ( $self, @strings ) {
-	$self->_send_it( $self->_output_fh, join "\n", @strings );
+	$self->send_it( $self->output_fh, join "\n", @strings );
 	}
 
-sub _error_fh ( $self ) { $self->{error} }
+sub output_fh ( $self ) { $self->{output_fh} }
 
-sub _output_fh ( $self ) { $self->{output} }
+=item * send_it( FILEHANDLE, MESSAGE )
 
-sub _send_it ( $self, $fh, $string ) {
-	print {$fh} $string
+The method that actually output the message. This will immediately return
+is the object is set to be quiet.
+
+This will add the value of the C<line_ending> setting to C<MESSAGE>.
+
+=cut
+
+sub send_it ( $self, $fh, $string ) {
+	return if $self->is_quiet;
+	$string .= $self->{'line_ending'};
+	$fh->print($string);
 	}
 
 =back
+
+=head2 Quiet
+
+=over 4
+
+=item * is_quiet
+
+Returns true if quiet is on, and false otherwise.
+
+=cut
+
+sub is_quiet ($self) { $self->{'quiet'} }
+
+=item * turn_off_quiet
+
+Diasble quiet mode. This does not mean that there will be output, but that the
+objects in C<error_fh> and C<output_fh> will get a change to handle the messages.
+
+=item * turn_on_quiet
+
+Enable quiet mode. The filehandle objects will not get a chance to handle the
+messages.
+
+=back
+
+=cut
+
+sub turn_off_quiet ($self) { $self->{'quiet'} = 0 }
+
+sub turn_on_quiet ($self) { $self->{'quiet'} = 1 }
 
 =head1 TO DO
 
