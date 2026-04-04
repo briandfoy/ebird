71ba3202d5183da991eb55101c024f2d1118a779
brian d foy <briandfoy@pobox.com>
2026-04-04 10:32:06 -0400

    output the species list in the example

--- examples/species-list.pl
+++ examples/species-list.pl
@@ -7,9 +7,12 @@ use eBird;
 
 my $ebird = eBird->new;
 
-my $region = eBird::Region->new( 'US-NY' )
+my $region = eBird::Region->new( 'US-NY' );
+printf "There are %d species\n", scalar grep { ! $_->hybrid } $ebird->product->species_in_region($region)->@*;
 
-my $tax_versions = $ebird->product->species_list($region);
-say Mojo::Util::dumper($tax_versions);
-
-say "Latest version is " . $ebird->taxonomy->latest_version;
+foreach my $species ( sort { $a->species_code cmp $b->species_code } $ebird->product->species_in_region($region)->@* ) {
+	state $n = 0;
+	my $inflated = $species->inflate($ebird);
+	next if $inflated->is_hybrid;
+	printf "%3d  %-8s -> %s\n", ++$n, $inflated->species_code, $inflated->common_name;
+	}
