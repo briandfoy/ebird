f7226a32c1e5a902ff396ac8e340d7853ac318a8
brian d foy <briandfoy@pobox.com>
2026-09-06 05:41:51 -0400

    silence builtin experimental warning

--- lib/eBird/Command/website.pm
+++ lib/eBird/Command/website.pm
@@ -4,8 +4,9 @@ no feature qw(module_true);
 
 package eBird::Command::website;
 use parent qw(eBird::Command);
+
+use experimental qw(builtin);
 use builtin qw(ceil);
-use experimental qw(builtin::ceil);
 
 use namespace::autoclean;
 
